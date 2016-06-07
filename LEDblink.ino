7a49b28f53406c7a21bd224e720bb2d309c6e8ca
cemami <cemami@ufl.edu>
2016-06-07 10:54:43 -0400

    Add files via upload

---
+++ LEDblink.ino
@@ -0,0 +1,18 @@
+int LEDPin = 13;
+int waitTimeOn = 90;
+int WaitTimeOff = 100;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT); 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED,HIGH);
+  delay(waitTime);
+  digitalWrite(LED,LOW);
+  delay(waitTime);
+
+}
